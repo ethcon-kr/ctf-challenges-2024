8aa13a824a5c402910ebd312fe486cb815a0a907
combab0 <combab0@graylab.team>
2024-06-13 18:47:55 +0900

    upload hidden flag shop

---
+++ hidden_flag_shop/src/HiddenFlagShop.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+contract HiddenFlagShop {
+    address private _owner;
+    uint256 private password;
+    mapping(address user => bool hasFlag) public flag;
+
+    error IncorrectPassword();
+    error InsufficientFunds();
+
+    constructor() {
+        _owner = msg.sender;
+        password = uint256(blockhash(block.number - 1));
+    }
+
+    function buyFlag(uint256 password_) external payable {
+        if (password != password_) revert IncorrectPassword();
+        if (msg.value != 0.1 ether) revert InsufficientFunds();
+        password = uint256(blockhash(block.number - 1));
+        flag[msg.sender] = true;
+    }
+
+    function withdraw() external {
+        require(msg.sender == _owner);
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
