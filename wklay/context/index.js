9101be655dc45796592484f96d894fc317321557
combab0 <combab0@graylab.team>
2024-06-18 15:31:22 +0900

    upload wklay for template

---
+++ wklay/context/index.js
@@ -0,0 +1,24 @@
+'use client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { WagmiProvider } from 'wagmi'
+import { config, projectId } from '../config'
+
+const queryClient = new QueryClient()
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: false,
+  enableOnramp: false,
+})
+
+export default function Web3ModalProvider({ children, initialState }) {
+  return (
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
