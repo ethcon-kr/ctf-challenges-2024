11cfc5b72dbf94898e36202516786abc5c68e979
combab0 <combab0@graylab.team>
2024-08-01 20:43:51 +0900

    upload assembly

---
+++ assembly/src/Assembly.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+contract Assembly {
+    function flag(address) external view returns (bool) {
+        assembly {
+            mstore(0, sload(calldataload(4)))
+            return(0, 0x20)
+        }
+    }
+
+    function checker(uint256) external {
+        assembly ("memory-safe") {
+            if eq(mul(mload(0x40), shr(0xe0, calldataload(0))), xor(calldataload(4), shr(0xe0, shl(0xe0, caller())))) {
+                sstore(caller(), 1)
+            }
+        }
+    }
+}
